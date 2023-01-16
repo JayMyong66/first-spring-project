154c94cd2aeb7b1fa80097af91279bb7bba4c5fc
jayming66 <mjw8941@gmail.com>
2023-01-16 22:18:14 +0900

    code test - DB

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import com.example.sbb.question.Question;
+import com.example.sbb.question.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 }

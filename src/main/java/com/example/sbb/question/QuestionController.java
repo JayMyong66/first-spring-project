acfcbf5bb0de0a2bae88f157d70dee8a7beaa8bc
jayming66 <mjw8941@gmail.com>
2023-01-22 23:41:25 +0900

    modify QuestionController - add /question/create URL mapping

--- src/main/java/com/example/sbb/question/QuestionController.java
+++ src/main/java/com/example/sbb/question/QuestionController.java
@@ -29,4 +29,8 @@ public class QuestionController {
         model.addAttribute("question", question);
         return "question_detail";
     }
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
 }
